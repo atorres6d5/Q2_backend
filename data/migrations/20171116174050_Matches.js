1b32331b6da04b152cbe3b7433d50c436cf37376
Aldo Torres <atorres6d5@gmail.com>
2017-11-16 20:04:41 -0800

    took away null

--- data/migrations/20171116174050_Matches.js
+++ data/migrations/20171116174050_Matches.js
@@ -7,7 +7,7 @@ exports.up = function(knex, Promise) {
     table.integer('away_team').notNullable()
     //table.foreign('away_team').references('Teams.id')
     table.string('match_date', 50)
-    table.integer('result').notNullable()
+    table.integer('result')
     //table.foreign('result').references('Teams.id')
     table.timestamps(true, true)
     })
